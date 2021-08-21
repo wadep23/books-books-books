1390227d807484968c55b426b6307cbff9522861
Wade Facer <wadepfacer@gmail.com>
2021-08-21 16:07:24 -0600

    attempting to resolve build error

---
+++ build/precache-manifest.b9e12e4ee8859aaed5895034f8de4eb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "892c505ae534ec3765d6e9bf1b4cbc30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17965aae3d9840d183d8",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "6b69018fab423baa27f6",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "17965aae3d9840d183d8",
+    "url": "/static/js/2.22a351e1.chunk.js"
+  },
+  {
+    "revision": "b5901214077ea32a7534a1d4eff7c84a",
+    "url": "/static/js/2.22a351e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b69018fab423baa27f6",
+    "url": "/static/js/main.1f37e06a.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
