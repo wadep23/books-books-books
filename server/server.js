de908e23d739a5426044f03f5b513e7cc1c1659a
Wade Facer <wadepfacer@gmail.com>
2021-08-20 22:34:49 -0600

    fixed another build path

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ async function startApolloServer() {
   }
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    res.sendFile(path.join(__dirname, "/build/index.html"));
   });
 
   db.once("open", () => {
